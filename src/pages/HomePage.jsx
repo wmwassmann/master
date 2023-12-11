2c54ea0a5fc2248ef9190332ba1b68ef5c9114f9
William Wassmann <wmwassmann@hotmail.com>
2023-12-11 13:03:06 -0800

    Adding simple text and the like to home page. A little 'who am I?'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import "../css/globalStyle.css"
+import "../css/homePage.css"
 
 export default function homePage() {
     return (
-        <div className="header">
-            HOME PAGE
+        <div className="home-container">
+            <div className="home-info">
+                <div className="home-title left">
+                    software
+                </div>
+                <div className="home-article left">
+                    Two years experience in developing, testing, 
+                    and maintaining stable, scalable, and 
+                    secure automated financial systems
+                </div>
+            </div>
+            <div className="home-info">
+                <div className="home-title right">
+                    design
+                </div>
+                <div className="home-article right">
+                    Specializations in UX/UI integrations, platform upgrades, 
+                    and data retrieval and manipulations
+                </div>
+            </div>
         </div>
     )
 }
