f0b7a3e2f1ff3a8e3bec031490f2ff822c48f646
William Wassmann <wmwassmann@hotmail.com>
2024-01-30 13:55:23 -0800

    Added better email with fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "../../css/ComponentStyles/Contact/Contact.css"
-import ContactInfo from "./ContactInfo.jsx";
+import "../../css/ComponentStyles/Contact/contact.css"
+import ContactInfo from "../ContactInfo.jsx";
 import EmailForm from "../Emails/EmailForm.jsx"
 
 const ContactMain = () => {
