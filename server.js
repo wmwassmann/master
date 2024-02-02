2cb13a3307d840855767d0a67510f7aea9d96bc7
William Wassmann <wmwassmann@hotmail.com>
2024-02-01 17:26:05 -0800

    Updated looks

---
+++ server.js
@@ -0,0 +1,49 @@
+
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+// import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5173;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(cors({
+//   origin: 'http://localhost:5173'
+// }));
+
+const transporter = nodemailer.createTransport({
+  service: 'Outlook',
+  auth: {
+    user: '0', 
+    pass: '0' 
+  }
+});
+
+app.post('/master/contact', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const mailOptions = {
+    from: 'your_email@gmail.com',
+    to: 'recipient_email@example.com', 
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('SERVER Error sending email:', error);
+      res.status(500).send('SERVER Failed to send email');
+
+    } else {
+      console.log('SERVER Email sent:', info.response);
+      res.status(200).send('SERVER Email sent successfully');
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
