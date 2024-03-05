29604d193318f6adfc85f4319a71420de3224451
William Wassmann <wmwassmann@hotmail.com>
2024-03-04 19:25:52 -0800

    Testing something related to deployment and URLs

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,11 +3,8 @@ import "../css/PageStyles/Portfolio/portfolioStyles.css";
 import calcThumb from "../assets/calculatronThumb.jpg"
 
 const Portfolio = () => {
-
-  const baseUrl = "../../master/Projects";
-
   const [projects, setProjects] = useState([
-    { id: 1, title: 'Calculatron-5000', description: 'Calculator App', path: '/Calculatron-5000/FrontEnd/index.html', thumb: calcThumb},
+    { id: 1, title: 'Calculatron-5000', description: 'Calculator App', path: 'Projects/Calculatron-5000/FrontEnd/calculatron.html', thumb: calcThumb},
     { id: 2, title: 'Project 2', description: 'Description of Project 2', path: '/project2/index.html' },
     { id: 3, title: 'Project 3', description: 'Description of Project 3', path: '/project3/index.html' }
   ]);
@@ -19,7 +16,7 @@ const Portfolio = () => {
       </div>
       <div className='portfolio-inner-container'> 
         {projects.map(project => (
-          <a key={project.id} href={baseUrl + project.path} className='project-card'>
+          <a key={project.id} href="Projects/Calculatron-5000/FrontEnd/calculatron.html" className='project-card'>
             <div className='project-content'>
               <h3>{project.title}</h3>
               <p>{project.description}</p>
