de98ccbc13bfac1f17b402f723d1979458e6f511
William Wassmann <wmwassmann@hotmail.com>
2024-03-04 18:12:36 -0800

    Added a section which fetches the latest commits to my /master and parses the json into date, name and message. So this will likely be captured in the next deployment. Also used regex to specify yyyy/mm/dd on the date instead of the  full date.

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -46,6 +46,27 @@ const AboutMe = () => {
     }, 30);
   };
 
+  const [latestCommits, setLatestCommits] = useState([]);
+
+  useEffect(() => {
+    fetchLatestCommits();
+  }, []);
+
+  const fetchLatestCommits = async () => {
+    try {
+      const response = await fetch('https://api.github.com/repos/wmwassmann/master/commits');
+      const data = await response.json();
+      setLatestCommits(data.slice(0, 5).map(commit => {
+        const regex = /^(\d{4}-\d{2}-\d{2})T/;
+        const match = regex.exec(commit.commit.author.date);
+        const date = match ? match[1] : "Unknown Date";
+        return { ...commit, commitDate: date };
+      }));
+    } catch (error) {
+      console.error('Error fetching latest commits:', error);
+    }
+  };
+
   return (
     <div className="readme-container">
       <div className='aboutme-body'>
@@ -60,10 +81,17 @@ const AboutMe = () => {
               {typedText}
             </p>
           </div>
-        </div>
-          {/* <h1 className="my-name">
-            William Wassmann
-          </h1> */}
+            <div className="latest-commits">
+              <h2>Latest Commits</h2>
+              <ul>
+                {latestCommits.map(commit => (
+                  <li key={commit.sha}>
+                    <strong>{commit.commitDate} </strong><strong>{commit.commit.author.name}</strong>: {commit.commit.message}
+                  </li>
+                ))}
+              </ul>
+            </div>
+        </div>          
         <div className='about-me-skills-container'>
           <div className="flank right">
             <Skillsets />
