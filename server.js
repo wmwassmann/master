283094240c337eadccb15a08fcd1b78cfcc85232
William Wassmann <wmwassmann@hotmail.com>
2024-03-14 15:28:27 -0700

    (Homepage) Moving github button down a bit

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import mysql from 'mysql';
-import cors from 'cors';
+// import express from 'express';
+// import mysql from 'mysql';
+// import cors from 'cors';
 
-const app = express();
-app.use(cors());
-const PORT = 5173;
+// const app = express();
+// app.use(cors());
+// const PORT = 5173;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Thraximundar180_',
-  database: 'portfolio-database'
-});
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password:
+//   database: 'portfolio-database'
+// });
 
-db.connect(err => {
-  if (err) {
-    console.log("Error connecting to database:", err);
-    throw err;
-  }
-  console.log('Connected to Portfolio Database');
+// db.connect(err => {
+//   if (err) {
+//     console.log("Error connecting to database:", err);
+//     throw err;
+//   }
+//   console.log('Connected to Portfolio Database');
   
-  const sql = "SELECT `Portfolio Name` FROM `portfolio-items`";
-  db.query(sql, (err, results) => {
-    if (err) {
-      console.log("Error querying database:", err);
-      throw err;
-    }
-    console.log("Data retrieved from portfolio-items table:");
-    console.log(results);
-  });
-});
+//   const sql = "SELECT `Portfolio Name` FROM `portfolio-items`";
+//   db.query(sql, (err, results) => {
+//     if (err) {
+//       console.log("Error querying database:", err);
+//       throw err;
+//     }
+//     console.log("Data retrieved from portfolio-items table:");
+//     console.log(results);
+//   });
+// });
 
-app.get('/', (req, res) => {
-  return res.json("Hello - this is the backend")
-});
+// app.get('/', (req, res) => {
+//   return res.json("Hello - this is the backend")
+// });
 
-app.get('/portfolio/Tables/portfolio-items', (req, res) => { 
-  const sql = "SELECT * FROM portfolio-items";
-  db.query(sql, (err, data)=> {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
+// app.get('/portfolio/Tables/portfolio-items', (req, res) => { 
+//   const sql = "SELECT * FROM portfolio-items";
+//   db.query(sql, (err, data)=> {
+//     if (err) return res.json(err);
+//     return res.json(data);
+//   });
+// });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
